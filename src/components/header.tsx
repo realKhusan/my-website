7ad50536827cd96fc91128c54be5fbf4a908c7dc
Khusan Mirobidov <husanmirobidov@gmail.com>
2025-01-23 17:38:11 +0500

    refactor: update UI components and add new animations for improved user experience

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,42 +1,37 @@
 import React from "react";
 import { Button } from "./ui/button";
 import { Github, Moon } from "lucide-react";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "./ui/select";
 function Header() {
   return (
-    <div className="container fixed  left-0 right-0 z-10  top-5 ">
-      <div>
-        <div className="border-black/50 flex justify-between rounded-sm px-3 py-2 w-full bg-white/65 backdrop-blur-sm  border ">
-          <div>Header</div>
+    <div className="pb-20">
+      <div className="sm:container fixed w-full z-[100]   sm:left-0 sm:right-0 sm:z-10  sm:top-5 ">
+        <div>
+          <div className="border-black/20 flex justify-between sm:rounded-sm px-3 py-2 w-full bg-white/65 backdrop-blur-sm border-b  sm:border ">
+            <div>Header</div>
 
-          <div className="flex gap-2">
-            <Button
-              variant={"outline"}
-              size={"icon"}
-              className="ratio-[1/1] p-1"
-            >
-              <Github />
-            </Button>
-            <Button
-              variant={"outline"}
-              size={"icon"}
-              className="ratio-[1/1] p-1"
-            >
-              <Moon />
-            </Button>
-            <select className="border px-2 rounded-sm" name="" id="">
-              {menus.map((menu, index) => (
-                <option className="hover:bg-black" key={index} value={menu}>
-                  {menu}
-                </option>
-              ))}
-            </select>
+            <div className="flex gap-2">
+              <Button
+                variant={"outline"}
+                size={"icon"}
+                className="ratio-[1/1] p-1"
+              >
+                <Github />
+              </Button>
+              <Button
+                variant={"outline"}
+                size={"icon"}
+                className="ratio-[1/1] p-1"
+              >
+                <Moon />
+              </Button>
+              <select className="border px-2 rounded-sm" name="" id="">
+                {menus.map((menu, index) => (
+                  <option className="hover:bg-black" key={index} value={menu}>
+                    {menu}
+                  </option>
+                ))}
+              </select>
+            </div>
           </div>
         </div>
       </div>
@@ -44,6 +39,6 @@ function Header() {
   );
 }
 
-const menus = ["Home", "About", "Contact", "Blog"];
+const menus = ["UZ", "EN", "RU"];
 
 export default Header;
--- src/components/ui/splite.tsx
+++ src/components/ui/splite.tsx
@@ -1,26 +1,24 @@
-'use client'
+"use client";
 
-import { Suspense, lazy } from 'react'
-const Spline = lazy(() => import('@splinetool/react-spline'))
+import { Suspense, lazy } from "react";
+import { RoundSpinner } from "./spinner";
+const Spline = lazy(() => import("@splinetool/react-spline"));
 
 interface SplineSceneProps {
-  scene: string
-  className?: string
+  scene: string;
+  className?: string;
 }
 
 export function SplineScene({ scene, className }: SplineSceneProps) {
   return (
-    <Suspense 
+    <Suspense
       fallback={
         <div className="w-full h-full flex items-center justify-center">
-          <span className="loader"></span>
+          <RoundSpinner />
         </div>
       }
     >
-      <Spline
-        scene={scene}
-        className={className}
-      />
+      <Spline scene={scene} className={className} />
     </Suspense>
-  )
+  );
 }
